7d3936541071b8c6779b0c44c97b7173e90281d8
Nevan4 <Katspru45@gmail.com>
2025-03-12 22:37:01 +0100

    Add email_template.html to have a form for a email html structure in a separate file. Modify email_sender.py to use html template from a separate file.

--- email_sender.py
+++ email_sender.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from typing import List, Dict, Any
+from jinja2 import Environment, FileSystemLoader
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,6 +18,7 @@ class EmailSender:
         self.sender_email: str = os.getenv("SENDER_EMAIL")
         self.sender_password: str = os.getenv("SENDER_PASSWORD")
         self.recipient_emails: List[str] = self._load_emails_from_file(emails_file)
+        self.template_env = Environment(loader=FileSystemLoader(searchpath="templates"))
 
     @staticmethod
     def get_today_and_end_date(days_ahead: int) -> tuple:
@@ -39,106 +41,13 @@ class EmailSender:
     def create_email(self, movie_details: List[Dict[str, Any]], days: int, recipient_email: str) -> MIMEMultipart:
         """Create an email message with the movie details."""
         today, end_date = self.get_today_and_end_date(days)
+        template = self.template_env.get_template("email_template.html")
+        body = template.render(today=today.strftime('%Y-%m-%d'), end_date=end_date.strftime('%Y-%m-%d'), movie_details=movie_details)
+
         message = MIMEMultipart()
         message["From"] = self.sender_email
         message["To"] = recipient_email
         message["Subject"] = f"Repertuar nowych filmów na {today.strftime('%Y-%m-%d')} - {end_date.strftime('%Y-%m-%d')}"
-
-        # Start of the HTML body
-        body = """
-        <html>
-        <head>
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                    color: #333333;
-                }
-                h1 {
-                    color: #2a5d84;
-                }
-                .movie {
-                    border-bottom: 2px solid #cccccc;
-                    padding: 10px 0;
-                    margin: 20px 0;
-                }
-                .movie-title {
-                    font-size: 24px;
-                    color: #2a5d84;
-                }
-                .movie-details {
-                    margin: 10px 0;
-                    font-size: 14px;
-                }
-                .screening-times {
-                    background-color: #f0f0f0;
-                    padding: 10px;
-                    border-radius: 5px;
-                }
-                .screening-time {
-                    margin: 5px 0;
-                }
-            </style>
-        </head>
-        <body>
-        """
-
-        # Append the dynamic date range to the body
-        body += f"<h1>Nowe filmy na: {today.strftime('%Y-%m-%d')} - {end_date.strftime('%Y-%m-%d')}</h1>"
-
-        # Loop through each movie and build the HTML body
-        for movie in movie_details:
-            title = movie.get('title', 'No Title')
-            link = movie.get('link', 'No Link')
-            genre = movie.get('genre', 'No Genre')
-            description = movie.get('description', 'No Description')
-            production_year = movie.get('production_year', 'No Year')
-            screenings = movie.get('screening_times', [])
-
-            # Build the movie details block
-            body += f"""
-            <div class="movie">
-                <div class="movie-title">
-                    <a href="{link}" target="_blank" style="text-decoration: none; color: #007bff; font-weight: bold;">
-                        {title}
-                    </a>
-                </div>
-                <div class="movie-details">
-                    <strong>Gatunek:</strong> {genre}<br>
-                    <strong>Opis:</strong> {description}<br>
-                    <strong>Rok Produkcji:</strong> {production_year}
-                </div>
-                <div class="screening-times">
-                    <strong>Godziny Seansów:</strong>
-                    <div>
-            """
-
-            # Add screening times
-            added_screening = False
-            if screenings:
-                for screening in screenings:
-                    date = screening.get('date', 'No Date')
-                    times = ', '.join(screening.get('times', [])) if screening.get('times') else None
-
-                    if not times:
-                        continue
-
-                    body += f'<div class="screening-time">{date}: {times}</div>'
-                    added_screening = True
-
-                if not added_screening:
-                    body += "<div class='screening-time'>No upcoming screenings available</div>"
-            else:
-                body += "<div class='screening-time'>No screening times available</div>"
-
-            body += "</div></div></div>"
-
-        # End of the HTML body
-        body += """
-        </body>
-        </html>
-        """
-
-        # Attach the HTML body to the email message
         message.attach(MIMEText(body, "html"))
         return message
 
